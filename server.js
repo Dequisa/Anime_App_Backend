d249e85bb7af7d95077930d4003a10b86ab39d2f
Dequisa <Dequisas@gmail.com>
2022-02-06 12:04:33 -0500

    removed process.env in server.js  port config to use  for heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const app = require('./app')
-require('dotenv').config()
-const PORT = process.env.PORT
-
-app.listen(PORT, ()=>{
-    console.log('Listening on Port:', PORT)
-});
+const app = require("./app");
+require("dotenv").config();
+// const PORT = process.env.PORT
+app.listen(process.env.PORT || 3001);
+// app.listen(PORT, ()=>{
+//     console.log('Listening on Port:', PORT)
+// });
