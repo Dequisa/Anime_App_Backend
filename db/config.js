fed5ca5505532cc0b17edee5ec30bb6fcfb629f9
Dequisa <Dequisas@gmail.com>
2022-02-08 14:16:01 -0500

    Revert "edited db config to remove string and add config obj"
    This reverts commit 36b7d1518b6696455cfa73c4089f8dbd6ea148a1.

--- db/config.js
+++ db/config.js
@@ -3,15 +3,18 @@ const { request } = require("express");
 const pgp = require("pg-promise")();
 require("dotenv").config();
 
-const connectionObject = {
-  host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  database: process.env.PG_DB,
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  database_URL: process.env.DATABASE_URL,
-};
+// const connectionObject = {
+//   host: process.env.PG_HOST,
+//   port: process.env.PG_PORT,
+//   database: process.env.PG_DB,
+//   user: process.env.PG_USER,
+//   password: process.env.PG_PASSWORD,
+// };
 
-const db = pgp(connectionObject);
+// const db = pgp(connectionObject);
+
+const db = pgp(
+  "postgres://hpriqneedvfglh:9ad76d81cbb351049a4e87735bd36e683c38836bf4c74f2ba58cf1c9484c50f2@ec2-35-153-35-94.compute-1.amazonaws.com:5432/d5qk30i399c3d2"
+);
 
 module.exports = db;
