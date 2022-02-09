4afb94222bd9f1e4665fa39bb5fbe1d078b8ec4a
Dequisa <Dequisas@gmail.com>
2022-02-09 12:37:37 -0500

    added uri as paramerter in db/config.js

--- db/config.js
+++ db/config.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 
 const connectionObject = {
   host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
+  // port: process.env.PG_PORT,
   database: process.env.PG_DB,
   user: process.env.PG_USER,
   password: process.env.PG_PASSWORD,
